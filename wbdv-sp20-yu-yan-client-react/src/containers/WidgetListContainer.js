48fdbc901844a203affc66d5d2ef92ca002e16ce
Yu Yan <miracleyanyu@gmail.com>
2020-02-19 15:29:04 -0800

    Created WidgetListContainer

---
+++ wbdv-sp20-yu-yan-client-react/src/containers/WidgetListContainer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {connect} from "react-redux";
+import service from "../services/WidgetService";
+import {
+  findWidgetsForTopic,
+  createWidget
+} from "../actions/WidgetActions";
+import WidgetListComponent from "../components/Widget/WidgetListComponent";
+
+const stateToPropertyMapper = (state) => ({
+  widgets: state.widgets.widgets
+});
+
+const dispatchToPropertyMapper = (dispatch) => ({
+  createWidget: (topicId, widget) =>
+      service.createWidget(topicId, widget)
+      .then(actualWidget =>
+          dispatch(createWidget(actualWidget))),
+  findWidgetsForTopic: (topicId) =>
+      service.findWidgetsForTopic(topicId)
+      .then(widgets =>
+          dispatch(findWidgetsForTopic(widgets))),
+  updateWidget: (topicId, widgetId, widget) => {
+    service.updateWidget(widgetId, widget)
+    .then(status => {
+      return service.findWidgetsForTopic(topicId)
+    })
+    .then(widgets =>
+        dispatch(findWidgetsForTopic(widgets)))
+  }
+});
+
+const WidgetListContainer = connect(
+    stateToPropertyMapper,
+    dispatchToPropertyMapper
+)(WidgetListComponent);
+
+export default WidgetListContainer;
